a5b89f282b3950f34140869d65db59843b2c8dc6
depressivek1d <110107784+depressivek1d@users.noreply.github.com>
2022-07-27 21:57:42 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,16 +4,16 @@ import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
-            System.out.println("Введите выражение. Аргументы и знак должны быть разделены пробелом");
-            System.out.println("Программа принимает арабские и римские числа");
-            System.out.println("Числа от 0 до 10 включительно и от I до X включительно");
+ //Ya napisal na angliskom potomu chto y men9 problemy s kirilitsoy v idea( izvinite za neudobstva
+            System.out.println("Enter an expression. Arguments and sign must be separated by a space");
+            System.out.println("The program accepts Arabic and Roman numbers");
+            System.out.println("Numbers from 0 to 10 inclusive and from I to X inclusive");
 
             try (Scanner sc = new Scanner(System.in)) {
                 String result = new Calculator().calculate(sc.nextLine().trim().split(" "));
                 System.out.println(result);
             } catch (Exception e) {
-                System.out.println("Введены неверные данные -_-");
+                System.out.println("Invalid data entered -_-");
             }
 
         }
